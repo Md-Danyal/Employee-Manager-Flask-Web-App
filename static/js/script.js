1d3ba82857a566cd8a3f89fb971dc2f56316246a
Md-Danyal <themddanyal@gmail.com>
2026-03-23 14:02:01 +0530

    Updated JS BOM

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+let submit = ()=> {
+  alert("Employee Added Sucessfully...")
+}
+let update = ()=> {
+  alert("Employee data uploaded successfully...")
+}
+
+// let remove = ()=> {
+//   let res = confirm("Are you sure you want to delete the data permanently ?")
+//   if(res){
+//     let a = ()=> {
+//       link = "{{ url_for('delete_employee',id=employee.employee_id)}}"
+//     }
+//   }
+// }
